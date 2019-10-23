f3df55735b2cb38165a0d4f6f73c9db137ea50be
NessaPaixao <vanessadapaixaobarros1@gmail.com>
2019-10-23 15:52:04 -0300

    Adicionar estrutura da home

---
+++ src/componentes/Home/Home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Home.scss';
+import {Link} from 'react-router-dom';
+import Criança from '../imagens/Criança.jpg';
+
+const Home = () => {
+    return (
+<div >
+    <div className="Home">
+        <Link to="/Home"><>Home</></Link>
+        <Link to="/Parceria"><>Parceria</></Link>
+        <Link to="/Doar"><>Doar</></Link>
+        <Link to="/Cadastro"><button>Cadastro</button></Link>
+    </div>
+    <div className="container">
+
+    <div className="Imagem_1">
+        <img src={Criança} alt="Criança"></img>
+    </div>         
+
+        <div className="textoContainer">
+         <div className="texto">
+            <h1>Quem somos</h1>
+            <p>
+            O Instituto Livres é uma organização da sociedade civil sem fins lucrativos, que atua desde 2006 de forma efetiva para a transformação da vida de pessoas e comunidades em situação de vulnerabilidade e alto risco.
+            Em 2005, sensibilizado com a realidade do tráfico e exploração sexual infantil, Juliano Son se dispôs a gravar um CD com o propósito de promover e apoiar financeiramente projetos e ações que resgatam crianças desse sistema de violência e injustiça. Foi assim que nasceu o projeto musical de Juliano Son, hoje líder e vocalista da banda Livres
+            </p>
+         </div>
+    </div>
+    </div>
+</div>
+ )
+}
+
+export default Home;
