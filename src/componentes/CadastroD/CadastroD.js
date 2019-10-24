fe1e18c57c8cb37c42574ae5f4a9ceba69a6808a
matheus s. silva <viciogamer157@gmail.com>
2019-10-24 17:11:38 -0300

    Adicionar formulário de Doador

---
+++ src/componentes/CadastroD/CadastroD.js
@@ -0,0 +1,74 @@
+import React, { useState, } from "react";
+import Input from '../input/Input.js';
+import "../cadastroD/CadastroD.scss";
+
+
+const CadastroD = () => {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [idade, setIdade] = useState("");
+  const [cpf, setCpf] = useState("");
+  
+  const handleSubmit = e => {
+  }
+   return (
+     <div className="NovoCad" >
+     <div className="Cadastro">
+       <div className="Imagem">
+       <h1>Faça seu cadastro de Doador</h1>
+       </div>
+      <form onSubmit={handleSubmit}>
+        <Input
+            value={nome}
+            type="text"
+            label="Nome"
+            placeholder="Nome completo"
+            atualizarState={setNome}
+            obrigatorio
+         />
+
+        <Input
+           value={idade}
+           type="text"
+           label="Idade"
+           placeholder="Digite sua idade"
+           atualizarState={setIdade}
+         />
+        
+        <Input
+             value={cpf}
+             class="form-control cpf-mask"
+             type="text"
+             label="CPF"
+             placeholder="Digite o CPF do responsável da Escola"
+             atualizarState={setCpf}
+             obrigatorio
+           />
+         <Input
+           value={email}
+           type="email"
+           label="Email"
+           placeholder="Digite seu email"
+           atualizarState={setEmail}
+         />
+         <Input
+           value={senha}
+           type="password"
+           label="Senha"
+           placeholder="Digite sua senha"
+           atualizarState={setSenha}
+           obrigatorio
+         />
+
+
+         <button>Cadastrar</button>
+      </form>
+    </div>
+    </div>
+  );
+};
+
+
+
+export default CadastroD;
